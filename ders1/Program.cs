e4baa4d88d2f891d496817806af3d11f2cb92692
gulcanisleyen <glcn.isleyen94@gmail.com>
2019-01-31 00:46:06 +0300

    first commit

---
+++ ders1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Console.WriteLine("Size of int :" + sizeof(int) + " byte yer kaplar.Max:" +
+                int.MaxValue + " ; Min :" + int.MinValue);
+            Console.ReadKey();*/
+
+        }
+    }
+}
---
+++ ders11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 21;
+            int b = 10;
+            int c;
+
+            c = a + b;
+            Console.WriteLine("Line1 - Value of c is " + c);
+            c = a - b;
+            Console.WriteLine("Line2 - Value of c is " + c);
+            c = a * b;
+            Console.WriteLine("Line3 - Value of c is " + c);
+            c = a / b;
+            Console.WriteLine("Line4 - Value of c is " + c);
+            c = a % b;
+            Console.WriteLine("Line5 - Value of c is " + c);
+            c = a ++;
+            Console.WriteLine("Line6 - Value of c is " + c);
+            c = a --;
+            Console.WriteLine("Line7 - Value of c is " + c);
+
+            Console.ReadLine();
+
+        }
+    }
+}
